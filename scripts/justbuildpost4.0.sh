dca9c1987f0e260bd0a0d1b59963740d27ed9be7
Ravi Prakash <raviprak@altiscale.com>
2014-12-22 16:34:09 -0800

    Removing stale justbuild* scripts

--- scripts/justbuildpost4.0.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -ex
-if [[ -z "$HADOOP_VERSION"  ||  -z "$PIG_VERSION" || -z "$HIVE_VERSION" ]]; then
-   echo "HADOOP_VERSION, PIG_VERSION and HIVE_VERSION must be explicitly set in the environment"
-   exit 1
-fi
-mvn package assembly:single versions:set -DnewVersion=${ARTIFACT_VERSION} -DskipTests=true -Phadoop-2 -Dhadoop.version=${HADOOP_VERSION} -Dpig.version=${PIG_VERSION} -Dhive.version=${HIVE_VERSION}
