f2e93bbbd47ac17fd7f1bd36087a0b5111a8f503
Ravi Prakash <raviprak@altiscale.com>
2015-03-31 08:32:57 -0700

    Now tying version of hadoop.auth.version to hadoop.version and hcatalog.version to hive.version

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -3,4 +3,4 @@ if [[ -z "$HADOOP_VERSION"  ||  -z "$PIG_VERSION" || -z "$HIVE_VERSION" ]]; then
    echo "HADOOP_VERSION, PIG_VERSION and HIVE_VERSION must be explicitly set in the environment"
    exit 1
 fi
-mvn package assembly:single versions:set -DnewVersion=${ARTIFACT_VERSION} -DskipTests=true -Phadoop-2 -Dhadoop.version=${HADOOP_VERSION} -Dpig.version=${PIG_VERSION} -Dhive.version=${HIVE_VERSION} -Dhadoop.auth.version=${HADOOP_AUTH_VERSION}
+mvn package assembly:single versions:set -DnewVersion=${ARTIFACT_VERSION} -DskipTests=true -Phadoop-2 -Dhadoop.version=${HADOOP_VERSION} -Dpig.version=${PIG_VERSION} -Dhive.version=${HIVE_VERSION} -Dhadoop.auth.version=${HADOOP_VERSION} -Dhcatalog.version=${HIVE_VERSION}
