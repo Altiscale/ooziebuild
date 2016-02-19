756b515daf278aaf2dde0c97fa62e91d1e55e240
Ravi Prakash <raviprak@altiscale.com>
2016-02-19 15:02:12 -0800

    HD-1484. Upgrade sqoop in Oozie sharelib

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-if [[ -z "$HADOOP_VERSION"  ||  -z "$PIG_VERSION" || -z "$HIVE_VERSION" ]]; then
+if [[ -z "$HADOOP_VERSION"  ||  -z "$PIG_VERSION" || -z "$HIVE_VERSION" || -z "$SQOOP_VERSION" ]]; then
    echo "HADOOP_VERSION, PIG_VERSION and HIVE_VERSION must be explicitly set in the environment"
    exit 1
 fi
@@ -11,7 +11,7 @@ wget http://conjars.org/repo/org/pentaho/pentaho-aggdesigner-algorithm/5.1.5-jhy
 # Install these untrusted artifacts in our local maven cache so that we can build hive.
 mvn install:install-file -Dfile=pentaho-aggdesigner-algorithm-5.1.5-jhyde.jar -DgroupId=org.pentaho -DartifactId=pentaho-aggdesigner-algorithm -Dpackaging=jar -Dversion=5.1.5-jhyde
 
-mvn install assembly:single versions:set -DnewVersion=${ARTIFACT_VERSION} -Puber -DskipTests=true -Phadoop-2 -Dhadoop.version=${HADOOP_VERSION} -Dpig.version=${PIG_VERSION} -Dhive.version=${HIVE_VERSION}
+mvn install assembly:single versions:set -DnewVersion=${ARTIFACT_VERSION} -Puber -DskipTests=true -Phadoop-2 -Dhadoop.version=${HADOOP_VERSION} -Dpig.version=${PIG_VERSION} -Dhive.version=${HIVE_VERSION} -Dsqoop.version=${SQOOP_VERSION}
 
 # Discover the path of the local maven cache
 MAVEN_LOCAL_REPO=`mvn help:evaluate -Dexpression=settings.localRepository | egrep -v '[INFO]|Download'`
