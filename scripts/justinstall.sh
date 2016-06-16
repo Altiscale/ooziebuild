a37b2253e540cc79d1048390768212e98fe1bda8
sookim-altiscale <sookim@altiscale.com>
2016-06-16 08:06:26 -0700

    HD-1556: Add oozie-examples into oozie-client rpm

--- scripts/justinstall.sh
+++ scripts/justinstall.sh
@@ -10,6 +10,7 @@ mkdir -p --mode=0755 ${DEST_DIR}
 cd ${DEST_DIR}
 tar -xvzpf ${WORKSPACE}/oozie/distro/target/oozie-${ARTIFACT_VERSION}-distro/oozie-${ARTIFACT_VERSION}/oozie-client-${ARTIFACT_VERSION}.tar.gz
 cp -r  ${WORKSPACE}/oozie/distro/target/oozie-${ARTIFACT_VERSION}-distro/oozie-${ARTIFACT_VERSION}/conf/ oozie-client-${ARTIFACT_VERSION}
+cp ${WORKSPACE}/oozie/distro/target/oozie-${ARTIFACT_VERSION}-distro/oozie-${ARTIFACT_VERSION}/oozie-examples.tar.gz oozie-client-${ARTIFACT_VERSION}
 # Make the Client RPM
 
 export RPM_NAME=alti-oozie-client-${ARTIFACT_VERSION}
